553188945c0a0085d57c70009bb15546506d8472
Navtech <navtech@Navtechs-MacBook-Air.local>
2025-08-13 17:48:41 +0530

    web form changes are added including date validation and auto fetch shop

--- airplane_mode/airport_shop_management/doctype/shop_at_airport/shop_at_airport.js
+++ airplane_mode/airport_shop_management/doctype/shop_at_airport/shop_at_airport.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2025, Navtech and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Shop At Airport", {
-// 	refresh(frm) {
+// Copyright (c) 2025, Navtech and contributors
+// For license information, please see license.txt
 
-// 	},
-// });
+frappe.ui.form.on('Shop At Airport', {
+  setup: function(frm) {
+    frm.set_query('shop_type', function() {
+      return {
+        filters: {
+          enabled: 1
+        }
+      };
+    });
+  }
+});
+

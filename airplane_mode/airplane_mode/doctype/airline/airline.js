7d47324166e747915e8e5154551805ad69eb15fa
Navtech <navtech@Navtechs-MacBook-Air.local>
2025-08-21 16:08:19 +0530

    added visit website clickable web link

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2025, Navtech and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on('Airline', {
+    refresh: function (frm) {
+        // Showing web link only if the website field is filled
+        if (frm.doc.website) {
+            frm.add_web_link(frm.doc.website, 'Visit Website');
+        }
+    }
+});
